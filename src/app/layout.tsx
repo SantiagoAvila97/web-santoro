33c7f1dbf4b7c2757489ee57b6708c3ffd6070a5
Santiago Avila Get <savila@globalenterprise.com.co>
2026-03-31 22:23:09 -0500

    feat:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script"; // 1. Importa esto
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,35 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        {/* 2. El script de Meta Pixel va aquí adentro */}
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1272556344262121');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </head>
-      <body className="min-h-full flex flex-col">
-        {children}
-        {/* El noscript es opcional, pero si lo pones, va así */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1272556344262121&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
